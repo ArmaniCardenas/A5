0076452c5cd7f079d6657728de29a27784fcd89d
ArmaniCardenas <59777924+ArmaniCardenas@users.noreply.github.com>
2024-12-11 20:09:17 -0800

    combined

--- Front-end/grid.js
+++ Front-end/grid.js
@@ -27,3 +27,44 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
 })
+
+
+const input = document.querySelector("#input");
+const city = document.querySelector("#city");
+const city_Name = document.querySelector("#cityName");
+const temp = document.querySelector("#temp");
+const main = document.querySelector("#main");
+const description = document.querySelector("#discription");
+const img = document.querySelector("#image");
+
+
+input.onsubmit = (e) => {
+    e.preventDefault();
+    weatherUpdate(city.value);
+    city.value = "";
+};
+
+weatherUpdate = (city) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=78d47e8abbb7da1d2fb25cc92917c4ed`);
+
+    xhr.send();
+    xhr.onload = () => {
+        if(xhr.status === 404) {
+            alert("Not found")
+        }
+        
+        else {
+            var data = JSON.parse(xhr.response);
+            cityName.innerHTML = data.name;
+            temp.innerHTML = `${Math.round(data.main.temp - 273.15)}°C`;
+            main.innerHTML = data.weather[0].main;
+            description.innerHTML = data.weather[0].description;
+            img.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+        }
+    };
+};
+
+
+weatherUpdate("irvine");
